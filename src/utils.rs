165357d3386bb0b4d0c25f5945b1583e63c1c075
Diego M. Pousa <diegopousalp@gmail.com>
2026-05-02 14:05:59 -0300

    Ejercicio 3

--- src/utils.rs
+++ src/utils.rs
@@ -41,3 +41,24 @@ fn es_primo_con_un_primo() {
 fn es_primo_con_un_no_primo() {
   assert!(!es_primo(8))
 }
+
+
+// Ejercicio 3
+fn suma_pares(lista: &[i32]) -> i32 {
+  lista
+    .iter()
+    .fold(0, |acc, item| if es_par(*item) { acc + item } else { acc } )
+}
+
+#[test]
+fn suma_pares_con_solo_pares() {
+  assert_eq!(suma_pares(&[32, 42, 2]), 76);
+}
+#[test]
+fn suma_pares_con_unos_impares() {
+  assert_eq!(suma_pares(&[7, 32, 3, 5, 42]), 74);
+}
+#[test]
+fn suma_pares_sin_pares() {
+  assert_eq!(suma_pares(&[5, 47, 25]), 0);
+}
