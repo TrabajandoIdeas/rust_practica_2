6680560eb49a6b849f96e7089d31f341cf1850a3
Diego M. Pousa <diegopousalp@gmail.com>
2026-05-02 13:55:40 -0300

    Ejercicio 2

--- src/utils.rs
+++ src/utils.rs
@@ -12,3 +12,32 @@ fn es_par_con_numero_impar() {
   assert_eq!(es_par(23), false);
 }
 
+
+// Ejercicio 2
+pub fn es_primo(numero: i32) -> bool {
+  if numero <=0 || es_par(numero)  {
+    return false;
+  }
+  
+  let mut count: i32 = 3;
+  let limit: i32 = numero / 2;
+  loop {
+    if count == limit {
+      break true;
+    }
+    if (numero % count) == 0 {
+      break false;
+    }
+
+    count += 2;
+  }
+}
+
+#[test]
+fn es_primo_con_un_primo() {
+  assert!(es_primo(7))
+}
+#[test]
+fn es_primo_con_un_no_primo() {
+  assert!(!es_primo(8))
+}
