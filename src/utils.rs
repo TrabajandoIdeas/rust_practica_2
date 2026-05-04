50aace8b1bc0ef119170a709a76405747f32f716
Diego M. Pousa <diegopousalp@gmail.com>
2026-05-04 06:19:37 -0300

    Ejercicio 7

--- src/utils.rs
+++ src/utils.rs
@@ -138,3 +138,30 @@ fn longitud_de_cadenas_con_3_cadenas() {
   assert_eq!(longitud_de_cadenas(&[String::from("Hola"), String::from(" "), String::from("Mundo")]), vec![4, 1, 5])
 }
 
+
+// Ejercicio 7
+fn cantidad_de_mayores(lista: &[i32], limite: i32) -> u32 {
+  let mut count = 0;
+
+  for item in lista {
+    if *item > limite {
+      count += 1;
+    }
+  }
+
+  return count;
+}
+
+#[test]
+fn cantidad_de_mayores_con_variedad() {
+  assert_eq!(cantidad_de_mayores(&[32, 23,65,2,4,40], 30), 3);
+}
+#[test]
+fn cantidad_de_mayores_cont_todos_mayores() {
+  assert_eq!(cantidad_de_mayores(&[32, 23,65,2,4,40], 1), 6);
+}
+#[test]
+fn cantidad_de_mayores_cont_todos_menores() {
+  assert_eq!(cantidad_de_mayores(&[32, 23,65,2,4,40], 70), 0);
+}
+
